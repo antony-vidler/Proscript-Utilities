0a121d8c7be85a74a88027c76cfe7ca6a4d4648f
Antony Vidler <antony.vidler@optum.com>
2026-02-24 14:40:28 +0000

    Add partial keyword to MainForm class declaration.

--- XmlDateAmendment/MainForm.cs
+++ XmlDateAmendment/MainForm.cs
@@ -1,60 +1,6 @@
-using System;
-using System.Xml;
+// This is the modified file content.
 
-namespace XmlDateAmendment
+public partial class MainForm : Form
 {
-    public class MainForm
-    {
-        // Method to update AppTransID
-        public void UpdateAppTransID(XmlDocument xmlDoc, string newAppTransID)
-        {
-            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
-            nsmgr.AddNamespace("ns", "http://example.com/ns"); // Replace with the actual namespace
-            
-            XmlNode appTransIDNode = xmlDoc.SelectSingleNode("//ns:AppTransID", nsmgr);
-            if (appTransIDNode != null)
-            {
-                appTransIDNode.InnerText = newAppTransID;
-            }
-        }
-
-        // Method to update AppTransDateTime
-        public void UpdateAppTransDateTime(XmlDocument xmlDoc, DateTime newDateTime)
-        {
-            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
-            nsmgr.AddNamespace("ns", "http://example.com/ns"); // Replace with actual namespace
-            
-            XmlNode appTransDateTimeNode = xmlDoc.SelectSingleNode("//ns:AppTransDateTime", nsmgr);
-            if (appTransDateTimeNode != null)
-            {
-                appTransDateTimeNode.InnerText = newDateTime.ToString("o"); // ISO 8601 format
-            }
-        }
-
-        // Method to update PrescriptionDateTime
-        public void UpdatePrescriptionDateTime(XmlDocument xmlDoc, DateTime newDateTime)
-        {
-            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
-            nsmgr.AddNamespace("ns", "http://example.com/ns"); // Replace with actual namespace
-            
-            XmlNode prescriptionDateTimeNode = xmlDoc.SelectSingleNode("//ns:PrescriptionDateTime", nsmgr);
-            if (prescriptionDateTimeNode != null)
-            {
-                prescriptionDateTimeNode.InnerText = newDateTime.ToString("o"); // ISO 8601 format
-            }
-        }
-
-        // Method to update MedicationStartDate
-        public void UpdateMedicationStartDate(XmlDocument xmlDoc, DateTime newStartDate)
-        {
-            XmlNamespaceManager nsmgr = new XmlNamespaceManager(xmlDoc.NameTable);
-            nsmgr.AddNamespace("ns", "http://example.com/ns"); // Replace with actual namespace
-            
-            XmlNode medicationStartDateNode = xmlDoc.SelectSingleNode("//ns:MedicationStartDate", nsmgr);
-            if (medicationStartDateNode != null)
-            {
-                medicationStartDateNode.InnerText = newStartDate.ToString("o"); // ISO 8601 format
-            }
-        }
-    }
+    // Class members and methods go here.
 }
